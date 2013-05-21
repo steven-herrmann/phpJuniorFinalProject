ef8bc8f591064518ba25603894a00b4d0a5955c0
mazzzzz <max.blackwolf@gmail.com>
2013-05-21 11:16:32 -0600

    Added new config files/connect script

---
+++ Configuration.php
@@ -0,0 +1,11 @@
+<?php
+namespace JProjFinal;
+
+class Configuration
+{
+    const mysql_username = "root";
+    const mysql_password = "";
+
+    const mysql_hostname = "localhost";
+    const mysql_database = "";
+}
---
+++ Includes/connect.php
@@ -0,0 +1,23 @@
+<?php
+use \JProjFinal\Configuration;
+
+try
+{
+    dibi::connect(array(
+        'driver'   => 'mysqli',
+        'host'     => Configuration::mysql_hostname,
+        'username' => Configuration::mysql_username,
+        'password' => Configuration::mysql_password,
+        'database' => Configuration::mysql_database,
+        'options'  => array(
+            MYSQLI_OPT_CONNECT_TIMEOUT => 30
+        ),
+        'flags'    => MYSQLI_CLIENT_COMPRESS,
+    ));
+    echo 'OK';
+
+}
+catch (DibiException $e)
+{
+    echo get_class($e), ': ', $e->getMessage(), "\n";
+}
