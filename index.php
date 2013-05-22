382cfcddf791a6aa1ceb97c5f5683931bc5eac5d
mazzzzz <max.blackwolf@gmail.com>
2013-05-22 10:52:45 -0600

    Fixed querystring routing

--- index.php
+++ index.php
@@ -9,6 +9,12 @@ $scriptPath = trim(preg_replace('/index\.[a-z]+/i', '', $_SERVER['SCRIPT_NAME'])
 
 $path = trim(str_replace("$scriptPath/", '', $_SERVER['REQUEST_URI']), '/');
 
+// Remove query string
+$queryStart = strpos($path, '?');
+if ($queryStart !== false)
+    $path = substr($path, 0, $queryStart);
+
+// Simple routing
 if (empty($path))
     Views::render('index');
 
