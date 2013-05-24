aa3daecd26f91ede31cecd50d559d61ac8cf4b9e
steven-herrmann <steven.herrmann@mtchs.org>
2013-05-24 12:20:47 -0500

    Added validation, changed form fields, WIP

--- views/employees/add.php
+++ views/employees/add.php
@@ -6,21 +6,14 @@
 <h1>Re: Add Employee</h1>
 
 <?php
-// Get routes
-$routes = dibi::fetchAll('SELECT * FROM route');
-
-
-$errorMessages = array();
-if (isset($_POST['submit']))
-{    
+if (isset($_POST))
+{
+    // Address
+    // Route
+    
     $errors = array();
 
-    // CSRF
-    if (!\NoCSRF::check('addclient_nonce', $_POST, false, 60 * 10, false, true))
-        $errors['form'][] = 'Error submitting, please try again.';
-
     // Firstname
-    $firstname = null;
     if (!isset($_POST['firstname']))
         $errors['firstname'][] = 'Firstname is required.';
     else
@@ -33,7 +26,6 @@ if (isset($_POST['submit']))
     }
 
     // Lastname
-    $lastname = null;
     if (!isset($_POST['lastname']))
         $errors['lastname'][] = 'Lastname is required.';
     else
@@ -44,97 +36,73 @@ if (isset($_POST['submit']))
         else if (strlen($lastname) > 50)
             $errors['lastname'][] = 'Lastname is too long.';
     }
-
-    // Address
-    $address = null;
-    if (!isset($_POST['address']))
-        $errors['address'][] = 'Address is required.';
+    
+    //Position
+    if(!isset($_POST['position']))
+        $errors['position'][] = "Position is a required field";
+    else
+        $position = $_POST['position'];
+    
+    //Salary
+    if(!isset($_POST['salary']))
+        $errors['salary'][] = "Salary is a required field";
     else
     {
-        $address = $_POST['address'];
-        if (strlen($address) == 0)
-            $errors['address'][] = 'Address cannot be empty.';
-        else if (strlen($address) > 50)
-            $errors['address'][] = 'Address is too long.';
-    }
-
-
-    // Route
-    $route = null;
-    if (isset($_POST['route']))
-    {
-        // If the route actually exists
-        foreach ($routes as $r)
-        {
-            if ($r->ID == $_POST['route'])
-                $route = $r->ID;
-        }
-    }
-
-
-    $errorMsgs = array();
-    foreach ($errors as $name => $errorList)
-        $errorMsgs[$name] = implode('  ', $errorList);
-
-    if (count($errorMsgs) == 0)
-    {
-        var_dump(array('firstname'=>$firstname, 'lastname'=>$lastname, 'address'=>$address, 'route'=>$route));
-        $result = dibi::insert('employee', compact($firstname, $lastname, $address, $route));
-        if ($result)
-        {
-            echo 'Employee Added.';
-            $_POST = array();
-        }
-        else
-            echo 'Error adding employee.';
+        $salary = $_POST['salary'];
+        $replaceArr = array("$",",");
+        $salary = str_replace();
     }
 }
+
+$posArr = array(
+    "Manager",
+    "Driver",
+    "Retriever",
+    "Clerk",
+    "Cashier",
+    "Sorter"
+    )
+// Get routes
 ?>
 
-<?php av($errorMsgs, 'form'); ?>
-<form action="<?php echo htmlspecialchars($_SERVER['REQUEST_URI']); ?>" method="post">
-    <input type="hidden" name="addclient_nonce" value="<?php echo \NoCSRF::generate('addclient_nonce'); ?>" />
+
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+    <input type="hidden" name="_nonce" value="<?php \NoCSRF::generate('addEmployee'); ?>" />
 
     <label class="form-required">Firstname:
-    <input type="text" class="first-name" name="firstname" value="<?php av($_POST, 'firstname'); ?>" required />
+    <input type="text" class="first-name" name="first-name" required />
     </label>
-    <?php if (($msg = av($errorMsgs, 'firstname', false, false)) !== false) : ?>
-        <p><?php echo htmlspecialchars($msg); ?>
-    <?php endif; ?>
 
     <label class="form-required">Lastname:
-    <input type="text" class="last-name" name="lastname" value="<?php av($_POST, 'lastname'); ?>" required />
+    <input type="text" class="last-name" name="last-name" required />
     </label>
-    <?php if (($msg = av($errorMsgs, 'lastname', false, false)) !== false) : ?>
-        <p><?php echo htmlspecialchars($msg); ?>
-    <?php endif; ?>
 
-    <label class="form-required">Address:
-    <input type="text" class="address" name="address" value="<?php av($_POST, 'address'); ?>" required />
-    </label>
-    <?php if (($msg = av($errorMsgs, 'address', false, false)) !== false) : ?>
-        <p><?php echo htmlspecialchars($msg); ?>
-    <?php endif; ?>
-
-    <label>Route:
-        <select name="route" class="route">
-            <?php if (count($routes) > 0) : ?>
-                <?php foreach ($routes as $route) : ?>
-                    <?php $selected = av($_POST, 'route', '', false) == $route->ID ? ' selected':''; ?>
-                    <option value="<?php echo $route->ID; ?>"<?php echo $selected; ?>><?php htmlspecialchars($route->RouteName); ?></option>
-                <?php endforeach; ?>
+    <label class="form-required">Position:
+        <select name="position" class="posiiton">
+            <?php if (count($posArr) > 0) : ?>
+                <?php for($i; $i < count($posArr); $i++):?>
+                    <option value="<?php echo $posArr[$i]; ?>"><?php htmlspecialchars($posArr[$i]); ?></option>
+                <?php endfor; ?>
             <?php else: ?>
-                <option>No Routes to choose from.</option>
+                <option>No positions to choose from.</option>
             <?php endif; ?>
         </select>
     </label>
-    <?php if (($msg = av($errorMsgs, 'route', false, false)) !== false) : ?>
-        <p><?php echo htmlspecialchars($msg); ?>
-    <?php endif; ?>
-
-
+    
+    <label class="form-required">Salary:
+        <input type="text" class="salary" name="salary" required/>
+    </label>
+    
+    <label>Manager:
+        <input type="text" class="manager" name="manager" />
+    </label>
+    
     <input name="submit" class="button form-button" type="submit" value="Submit" />
     <input name="reset" class="button form-button" type="reset" value="Clear" />
 </form>
+
+<?php
+$result = dibi::fetchAll('SELECT *, manager.FirstName as ManagerFirstName, manager.LastName as ManagerLastName FROM employee RIGHT JOIN employee as manager ON manager.ID = employee.ManagerID');
+?>
 
 <?php Views::renderPart('template/footer'); ?>
