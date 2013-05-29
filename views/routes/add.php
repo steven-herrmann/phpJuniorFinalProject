2f706740dd09b0cf16dd9cd6c69fefb0faeec554
steven-herrmann <steven.herrmann@mtchs.org>
2013-05-29 11:26:31 -0500

    Updated form

--- views/routes/add.php
+++ views/routes/add.php
@@ -6,8 +6,8 @@
 <h1>Re: Add Routes</h1>
 
 
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-    <input type="hidden" name="_nonce" value="<?php \NoCSRF::generate('addRoute'); ?>" />
+<form action="<?php echo htmlspecialchars($_SERVER['REQUEST_INI']); ?>" method="post">
+    <input type="hidden" name="addRoute_nonce" value="<?php \NoCSRF::generate('addRoute_nonce'); ?>" />
 
     <label class="form-required">Route Name
     <input type="text" class="route-name" name="route-name" required />
@@ -19,7 +19,7 @@
           $result = dibi::fetchAll('SELECT id FROM truck');
           foreach($result as $row):
         ?>
-           <option value = "<?php echo $row;?>"> <?php echo $row;?></option>
+           <option value = "<?php echo htmlspecialchars($row);?>"> <?php echo htmlspecialchars($row);?></option>
         <?php endforeach; ?>
       </select>
     
